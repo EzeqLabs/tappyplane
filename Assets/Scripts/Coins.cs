d98918b8bcffeb9f2ca2fc76c24d596669b9acb5
Kako Botasso <kakodev@gmail.com>
2016-08-27 19:40:56 -0300

    Adicionando moedas

---
+++ Assets/Scripts/Coins.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Coins : MonoBehaviour {
+	private int coins = PlayerPrefs.GetInt ("Coins");
+	public Text coinsText;
+
+	void Start(){
+		DrawCoins ();
+	}
+
+	void Update() {
+		DrawCoins ();
+	}
+
+	public void IncrementAndDrawCoins(){
+		this.coins = PlayerPrefs.GetInt ("Coins");
+		this.coins += 1;
+		PlayerPrefs.SetInt ("Coins", coins);
+		DrawCoins ();
+	}
+
+	public void DrawCoins(){
+		coinsText.text = coins.ToString();
+	}
+
+	int GetCoins(){
+		return PlayerPrefs.GetInt ("Coins");
+	}
+}
